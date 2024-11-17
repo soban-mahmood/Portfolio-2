741679218f4e363741e74e62abad9249bfde12fe
Soban Mehmood <sobanmehmood4343@gmail.com>
2024-11-16 23:14:33 -0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import './App.css'
 import Page1 from "./pages/Page1";
+import Header from "./components/Header";
 const App = () => {
   
   return (
     <>
+    <Header/>
     <Page1/>
     </>
   );
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='absolute w-full flex items-center justify-end z-10 p-20'>
+    <button className='bg-black px-10 py-2 rounded-full text-white border-[6px] hover:bg-gray-600'>Hire me</button>
+    </div>
+  )
+}
+
+export default Header
