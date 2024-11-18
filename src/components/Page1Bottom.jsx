84ab6a374c1b5d26ce68becd89deaa5862c0db3c
Soban Mehmood <sobanmehmood4343@gmail.com>
2024-11-18 11:51:27 -0600

    .

--- src/components/Page1Bottom.jsx
+++ src/components/Page1Bottom.jsx
@@ -3,7 +3,7 @@ import logo1 from "./../assets/sidelogo1.webp";
 import logo2 from "./../assets/sidelogo2.webp";
 const Page1Bottom = () => {
   return (
-    <div className="absolute bottom-0 w-full left-0 px-[7vw] py-[5vh] flex justify-between items-end">
+    <div className="absolute w-full left-0 px-[7vw] py-[5vh] flex justify-between items-end">
       <div>
         <h2 className="text-[25px] font-[anzo3]">BRAND DESIGN | WEBSITE DESIGN</h2>
         <h2 className="text-[25px] font-[anzo2] text-[#8C8C8C]"> BESPOKE FREELANCE</h2>
--- src/pages/Page1.jsx
+++ src/pages/Page1.jsx
@@ -7,19 +7,22 @@ const Page1 = () => {
   const titlRef = useRef(null);
 
   const mouseMove = (e) => {
-    console.log(titlRef.current.getBoundingClientRect());
+    // console.log(titlRef.current.getBoundingClientRect());
+    titlRef.current.style.transform = `rotateX(${e.clientX}deg)`;
   };
 
   return (
     <>
       <div
+        id="page1"
         onMouseMove={(e) => {
           mouseMove(e.clientX, e.clientY);
         }}
-        className="h-screen bg-white p-8 "
+        className="h-screen relative bg-white p-8  "
       >
         <div
-          className="h-full w-full p-10 bg-cover shadow-lg  shadow-gray-700 text-white relative rounded-[45px]"
+        id="page1-in"
+          className="h-full w-full p-10 bg-cover shadow-lg relative shadow-gray-700 text-white rounded-[45px]"
           style={{
             backgroundImage: `url(${PIC})`,
             backgroundPosition: "center",
@@ -27,24 +30,23 @@ const Page1 = () => {
             backgroundSize: "cover",
           }}
         >
-          <div className="px-20" id="page-in">
-            <img src={logo} alt="LOGO" className="h-32" />
-            <div ref={titlRef} id="tiltDiv" className="mt-40">
-              <h1 className="text-[4.2vw] uppercase font-[anzo1] leading-[4vw]">
-                I AM
-                <span className="text-black mx-4">
-                  Mern Stack <span className="text-white ">™</span>
-                </span>
-              </h1>
-              <h1 className="text-[8vw] leading-[7vw] font-[anzo1] uppercase">
-                Developer
-              </h1>
-              <h1 className="text-[4.2vw]  leading-[4vw] font-[anzo1]">
-                TO HIRE
-              </h1>
-            </div>
-            <Page1Bottom />
+          <img src={logo} alt="LOGO" />
+          <div ref={titlRef} id="tiltDiv" className="mt-40 ">
+            <h1 className="text-[4.2vw] uppercase font-[anzo1] leading-[4vw]">
+              I AM
+              <span className="text-black mx-4">
+                Mern Stack <span className="text-white ">™</span>
+              </span>
+            </h1>
+            <h1 className="text-[8vw] leading-[7vw] font-[anzo1] uppercase">
+              Developer
+            </h1>
+            <h1 className="text-[4.2vw]  leading-[4vw] font-[anzo1]">
+              TO HIRE
+            </h1>
+
           </div>
+            <Page1Bottom />
         </div>
       </div>
     </>
