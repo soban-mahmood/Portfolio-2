9ecb3a44c27abe00ecdf2b38c04a1c1f81218f03
Soban Mehmood <sobanmehmood4343@gmail.com>
2024-11-23 20:13:55 -0600

    .

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import './App.css'
 import Page1 from "./pages/Page1";
 import Header from "./components/Header";
+import Page2 from "./pages/Page2";
 const App = () => {
   
   return (
     <div id="main">
     <Header/>
     <Page1/>
+    <Page2/>
     </div>
   );
 };
---
+++ src/pages/Page2.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Page2 = () => {
+  return (
+    <div className='bg-white text-center text-black '>
+    <h4 className='text-2xl text-[#717171] font-[anzo2]'>
+    ©  Soban.Dev 2024 | designed and developed</h4>   
+    <div className=''>
+        <h1 className='text-[45vw] font-[anzo4]'>IMPACTFUL</h1>
+    </div>
+    <div className=''>
+        <h1 className='text-[45vw] font-[anzo4]'>DESIGN</h1>
+    </div>
+    <div className=''>
+        <h1 className='text-[45vw] font-[anzo4]'>IS THE</h1>
+    </div>
+    <div className=''>
+        <h1 className='text-[45vw] font-[anzo4]'>DESIGN</h1>
+    </div>
+    <div className=''>
+        <h1 className='text-[45vw] font-[anzo4]'>THATS</h1>
+    </div>
+    <div className=''>
+        <h1 className='text-[45vw] font-[anzo4]'>WORKS!</h1>
+    </div>
+    </div>
+  )
+} 
+
+export default Page2
