7683deca6067d4ec0d0db6efa1a118e594de2af6
Soban Mehmood <sobanmehmood4343@gmail.com>
2024-11-25 17:18:34 -0600

    ./

--- src/pages/Page6.jsx
+++ src/pages/Page6.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import video from "../assets/vedio.mp4";
 const Page6 = () => {
   return (
-    <div className="h-full p-10 ">
+    <div className="h-full relative p-10 ">
       <div className="h-screen w-[100%] rounded-[50px] bg-neutral-100">
         <video src={video} autoPlay muted loop className=" relative rounded-[45px] h-full w-full object-cover "></video>
-        <h1 className="uppercase absolute ">soban</h1>
+        <h1 className="uppercase absolute text-[30vw] font-[anzo4] px-10 bottom-0 text-white ">soban</h1>
       </div>
     </div>
   );
